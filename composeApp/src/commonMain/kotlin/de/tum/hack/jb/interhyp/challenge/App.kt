617ff94e971768e53d18df5b643cb94dd1dbc136
Illia Minkin <minkin050301@gmail.com>
2025-11-22 13:10:24 +0100

    Set up android and iOS environment

--- composeApp/src/commonMain/kotlin/de/tum/hack/jb/interhyp/challenge/App.kt
+++ composeApp/src/commonMain/kotlin/de/tum/hack/jb/interhyp/challenge/App.kt
@@ -6,42 +6,101 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import jb_interhyp_challenge.composeapp.generated.resources.Res
 import jb_interhyp_challenge.composeapp.generated.resources.compose_multiplatform
 
+// Custom Plane Icon
+val PlaneIcon: ImageVector
+    get() = ImageVector.Builder(
+        name = "Flight",
+        defaultWidth = 24.dp,
+        defaultHeight = 24.dp,
+        viewportWidth = 24f,
+        viewportHeight = 24f
+    ).apply {
+        path(fill = androidx.compose.ui.graphics.SolidColor(androidx.compose.ui.graphics.Color.Black)) {
+            moveTo(21f, 16f)
+            verticalLineToRelative(-2f)
+            lineToRelative(-8f, -5f)
+            verticalLineTo(3.5f)
+            curveToRelative(0f, -0.83f, -0.67f, -1.5f, -1.5f, -1.5f)
+            reflectiveCurveToRelative(-1.5f, 0.67f, -1.5f, 1.5f)
+            verticalLineTo(9f)
+            lineToRelative(-8f, 5f)
+            verticalLineToRelative(2f)
+            lineToRelative(8f, -2.5f)
+            verticalLineTo(19f)
+            lineToRelative(-2f, 1.5f)
+            verticalLineTo(22f)
+            lineToRelative(3.5f, -1f)
+            lineToRelative(3.5f, 1f)
+            verticalLineToRelative(-1.5f)
+            lineTo(13f, 19f)
+            verticalLineToRelative(-5.5f)
+            lineToRelative(8f, 2.5f)
+            close()
+        }
+    }.build()
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("My App") },
+                    actions = {
+                        IconButton(onClick = { /* Handle plane icon click */ }) {
+                            Icon(
+                                imageVector = PlaneIcon,
+                                contentDescription = "Plane"
+                            )
+                        }
+                    }
+                )
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.primaryContainer)
+                    .padding(paddingValues)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Button(onClick = { showContent = !showContent }) {
+                    Text("Click me!")
+                }
+                AnimatedVisibility(showContent) {
+                    val greeting = remember { Greeting().greet() }
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Image(painterResource(Res.drawable.compose_multiplatform), null)
+                        Text("Compose: $greeting")
+                    }
                 }
             }
         }
